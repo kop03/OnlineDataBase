2712e336bdccc8344acd25db8d97237164dad1c1
wuyanzu <atguigu@atguigu.com>
2024-05-25 16:19:47 +0800

    状态后端及检查点相关配置

--- realtime-common/src/test/java/Test01.java
+++ realtime-common/src/test/java/Test01.java
@@ -3,8 +3,11 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import static org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION;
 
 public class Test01 {
     public static void main(String[] args) {
@@ -12,9 +15,25 @@ public class Test01 {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(4);
 
-        // 2.添加检查点和状态后端
-//        env.enableCheckpointing(5000L);
-//        env.setStateBackend(new HashMapStateBackend());
+// 状态后端及检查点相关配置
+        // 1.4.1 设置状态后端
+        env.setStateBackend(new HashMapStateBackend());
+
+        // 1.4.2 开启 checkpoint
+        env.enableCheckpointing(5000);
+        // 1.4.3 设置 checkpoint 模式: 精准一次
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        // 1.4.4 checkpoint 存储
+        env.getCheckpointConfig().setCheckpointStorage("hdfs://hadoop101:8020/gmall2023/stream/" + "test01");
+        // 1.4.5 checkpoint 并发数
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        // 1.4.6 checkpoint 之间的最小间隔
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(5000);
+        // 1.4.7 checkpoint  的超时时间
+        env.getCheckpointConfig().setCheckpointTimeout(10000);
+        // 1.4.8 job 取消时 checkpoint 保留策略
+        env.getCheckpointConfig().setExternalizedCheckpointCleanup(RETAIN_ON_CANCELLATION);
+
 
         // 3.读取数据
         DataStreamSource<String> kafkaSource = env.fromSource(
